fe3298ff6042c2fcf8f643bcc703be63b0fa80fd
AshaTyb <155414250+AshaTyb@users.noreply.github.com>
2024-02-05 21:58:38 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿var number1 = 5;
+var number2 = 10;
+// ==
+// !=
+//<
+//>
+//<=
+//>=
+var name = "Adam";
+var age = 33;
+
+if (name == "Adam" || age < 50)
+{
+    Console.WriteLine("Jestem adamem przed 50");
+
+    // &&
+    // ||
+    //!
+}
